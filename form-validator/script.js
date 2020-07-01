8287b6baa30252ca28a91c036eefe64b4ea2b214
Rayven <rayven.burns@gmail.com>
2020-07-01 15:16:21 -0400

    form validator done

--- form-validator/script.js
+++ form-validator/script.js
@@ -20,28 +20,58 @@ function showSuccess(input) {
 }
 
 //Check if email is valid
-function isValidEmail(email) { //js email regex
+function checkEmail(input) { //js email regex
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 //Check required fields
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') { //trims out whitespace
-            showError(input)
-        } 
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
     })
 }
 
+//Check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
 
+//Check passwords match
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match');
+    }
+}
 
+//Get field name
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 //Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    checkRequired([username, email, password, password2])
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 })
 
 
